ffddf6805772636e78b01d92aa03c1ae2b70d790
ununique3 <j.horst77@gmail.com>
2022-09-30 11:14:30 -0600

    working on nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [home, setHome] = useState(true)
+  const [projects, setProjects] = useState(false)
+  const [resume, setResume] = useState(false)
+  const [links, setLinks] = useState(false)
+
+  const handleHome = () => {
+    setHome(true)
+    setProjects(false)
+    setResume(false)
+    setLinks(false)
+  }
+  const handleProjects = () => {
+    setHome(false)
+    setProjects(true)
+    setResume(false)
+    setLinks(false)
+  }
+  const handleResume = () => {
+    setHome(false)
+    setProjects(false)
+    setResume(true)
+    setLinks(false)
+  }
+  const handleLinks = () => {
+    setHome(false)
+    setProjects(false)
+    setResume(false)
+    setLinks(true)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <header>
+      <h1>Jason Horst</h1>
+      <nav>
+        <ul>
+          <li onClick={() => handleHome()}>Home</li>
+          <li onClick={() => handleProjects()}>Projects</li>
+          <li onClick={() => handleResume()}>Resume</li>
+          <li onClick={() => handleLinks()}>Links</li>
+        </ul>
+        <ul>
+          <li>Github</li>
+          <li>LinkedIn</li>
+        </ul>
+      </nav>
+    </header>
+      <Body />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+
+
+
+const Body = () => {
+
+
+
+
+  return (
+    <div>
+      <p>About me</p>
+    </div>
+  );
+}
+
+export default Body;
